e779bc9d7f2f7a66466f49d0c1ab192a2005c9da
Zephyre <laurelineduf@proton.me>
2025-02-12 17:11:41 +0100

    check argument ok et norminé

--- check_arguments/ft_check_only_num.c
+++ check_arguments/ft_check_only_num.c
@@ -6,7 +6,7 @@
 /*   By: lduflot <lduflot@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 16:20:19 by lduflot           #+#    #+#             */
-/*   Updated: 2025/02/12 16:20:43 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/02/12 17:11:14 by lduflot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,7 @@ void	ft_check_only_num(char *str)
 	while (str[i] != '\0')
 	{
 		if (!(str[i] >= '0' && str[i] <= '9'))
-		{
-			write(1, "Error\n", 6);
-			return ;
-		}
+			error(str);
 		i++;
 	}
 }
